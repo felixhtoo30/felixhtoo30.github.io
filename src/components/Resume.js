553d4152b2cae8e74d2dda17427189934a5e9a03
Felix Htoo <felixhtoo@FHs-MacPro-M3.local>
2025-07-24 01:37:45 +0700

    update: Changes in Resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -8,7 +8,11 @@ export class Resume extends React.PureComponent {
     const maxLevel = 7;
     const SkillBar = ({ title, detail, level }) => (
       <div className="mb-6">
-        <p className="pa-normal mb-2">{title}<br/>{detail}</p>
+        <p className="pa-normal mb-2">
+          {title}
+          <br />
+          {detail}
+        </p>
         <div className="pa-skill-bar w-10/12 flex">
           {[...Array(maxLevel)].map((el, i) => (
             <p
@@ -38,10 +42,28 @@ export class Resume extends React.PureComponent {
       <div className="my-5">
         <p className="font-bold text-lg">{dev.name}</p>
         <ul>
-          <li><strong>Duration</strong>: {dev.duration}</li>
-          <li><strong>Skills</strong>: {dev.skills}</li>
-          {(dev.remark) ? <li><strong>Remark</strong>: {dev.remark}</li> : ''}
-          {(dev.url) ? <li><a href={dev.url} className="pa-url">Check Website</a></li> : ''}
+          <li>
+            <strong>Duration</strong>: {dev.duration}
+          </li>
+          <li>
+            <strong>Skills</strong>: {dev.skills}
+          </li>
+          {dev.remark ? (
+            <li>
+              <strong>Remark</strong>: {dev.remark}
+            </li>
+          ) : (
+            ""
+          )}
+          {dev.url ? (
+            <li>
+              <a href={dev.url} className="pa-url">
+                Check Website
+              </a>
+            </li>
+          ) : (
+            ""
+          )}
         </ul>
         {/* <div className="mt-3 flex text-3xl text-primary">
           <i className="fab fa-html5 mx-2" />
@@ -72,7 +94,9 @@ export class Resume extends React.PureComponent {
             <img src={`${profileImg}`} alt="Felix Htoo" className="w-9/12" />
           </div>
           <div className="w-7/12">
-            <h1 className="text-3xl text-primary mb-8">Htoo Ant Hlaing (Felix)</h1>
+            <h1 className="text-3xl text-primary mb-8">
+              Htoo Ant Hlaing (Felix)
+            </h1>
             <div className="main-info mb-4">
               <h3 className="pa-subhead">Email</h3>
               <p className="pa-normal">felixhtoo30@gmail.com</p>
@@ -86,11 +110,15 @@ export class Resume extends React.PureComponent {
               <p className="pa-normal">https://linkedin.com/in/felixhtoo30</p>
             </div>
             <div className="main-info mb-4">
+              <h3 className="pa-subhead">GitHub</h3>
+              <p className="pa-normal">https://github.com/felixhtoo30</p>
+            </div>
+            <div className="main-info mb-4">
               <h3 className="pa-subhead">Address</h3>
-              {/* <p className="pa-normal">
-                No.53, 7th floor (B), Eain Gyi St., Pazundaung Tsp., Yangon
-              </p> */}
-              <p className="pa-normal">Yangon, Myanmar</p>
+              <p className="pa-normal">
+                No. 217, 3rd fl (B), Dhama Yarzar 28 St., 10th Ward, South Okkalapa Tsp., Yangon, 11091
+              </p>
+              {/* <p className="pa-normal">Bangkok, Thailand</p> */}
             </div>
           </div>
         </div>
@@ -100,7 +128,9 @@ export class Resume extends React.PureComponent {
           </div>
           <div className="w-8/12">
             <p className="pa-normal">
-              I would like to work as Wordpress Web Developer. I have around 6+ years hands-on experience with Web Development since 2017.
+              I would like to apply for Senior Full-stack Web Developer
+              position. I have around 7 years hands-on experience in Web
+              Development Industry since 2017.
             </p>
           </div>
         </div>
@@ -123,13 +153,44 @@ export class Resume extends React.PureComponent {
           </div>
           <div className="w-8/12">
             <ul>
-              <li>I build the websites, and develop themes & plugins</li>
-              <li>I solve the problems with user-centered approach.</li>
-              <li>I'm UXUI Design Enthusiast.</li>
-              <li> I've worked as Software Engineer about 1 year and Web Developer about 5+ years.</li>
-              <li> I've project experience with the following fields: Distribution, Warehouse, Cloud Reporting, Marketplace, Ecommerce, LMS Education, Hotel Booking, Event Booking and School Registration.</li>
-              <li> I usually use my free time by reading about Web technologies, Designs and honing my Programming knowledge and skills.</li>
-              <li> My Hobbies are Reading, Listening Podcasts, Learning, Psychology, Philosophy, Philanthropy and Design.</li>
+              <li>
+                I'm currently working as full-time remote as Senior Full-stack
+                Web Developer for ConnectingDNA Global Pte. Ltd.
+              </li>
+              <li>
+                As a remote experience, I have around 4 years hands on
+                experience about developing the websites since 2022, negotiating and
+                communicating with the foreign clients (London, Singapore,
+                India, Dubai, Malaysia, and Taiwan).
+              </li>
+              <li>
+                I've worked as Software Engineer about 1 year, and Web Developer
+                about 6 years: 3 years as Wordpress Developer and 3 years as
+                Full-stack Web Developer.
+              </li>
+              <li>
+                I've projects experience with the following fields:
+                Distribution, Warehouse, Cloud Reporting, Marketplace,
+                E-commerce, Real Estate, LMS Education, Hotel Booking, Event
+                Booking and School Registration.
+              </li>
+              <li>
+                I enjoy building the websites, developing themes & plugins
+              </li>
+              <li>I'm a problem solver with can-do attitude</li>
+              <li>I'm also a UXUI Design Enthusiast.</li>
+              <li>
+                I usually use my free time by reading about Web technologies,
+                UXUI Designs and honing my Programming knowledge and skills.
+              </li>
+              <li>
+                I usually read the books, listening the podcasts, and learning
+                something as usual.
+              </li>
+              <li>
+                My favorite topics are Design, Philanthropy, Maths, English,
+                Psychology and Self-improvement.
+              </li>
             </ul>
           </div>
         </div>
@@ -141,19 +202,26 @@ export class Resume extends React.PureComponent {
             <p className="pa-subhead pt-2 mb-6">Hard-skills</p>
             <div className="pa-flex-wrapper">
               {/* Level - 7 max. */}
-              <SkillBar title="HTML5/CSS3" level={6} />
-              <SkillBar title="SCSS/SASS" level={5} />
-              <SkillBar title="JQuery" level={6} />
-              <SkillBar title="Reactjs" level={4} />
-              <SkillBar title="Nodejs" level={3} />
-              <SkillBar title="Laravel" level={4} />
+              <SkillBar title="HTML5/CSS3" level={8} />
+              <SkillBar title="SCSS/SASS" level={7} />
+              <SkillBar title="JQuery" level={7} />
               <SkillBar title="Wordpress" level={6} />
-              <SkillBar title="Magento" level={3} />
-              <SkillBar title="Figma/Adobe XD" level={5} />
-              <SkillBar title="Adobe Photoshop/Illustrator" level={4} />
+              <SkillBar title="Self-Hosting" level={6} />
+              <SkillBar title="AWS" level={5} />
+              <SkillBar title="Laravel" level={4} />
+              <SkillBar title="Shopify" level={4} />
+              <SkillBar title="Magento" level={4} />
+              <SkillBar title="ReactJS" level={8} />
+              <SkillBar title="NodeJS" level={7} />
+              <SkillBar title="UXUI Design" level={4} />
             </div>
             <p className="pa-subhead pt-4 mb-4">Soft-skills</p>
-            <p>Independent . Motivated . Organized . Teamwork . Problem solving . Self-taught . Adaptability . Responsibility</p>
+            <p>
+              {" "}
+              Independent . Self-Motivated . Client Communication . Negotiation
+              . Organized . Leadership . Teamwork . Problem solving . Self-taught .
+              Adaptability . Responsibility
+            </p>
           </div>
         </div>
         <div className="flex mb-10" id="experience">
@@ -176,7 +244,7 @@ export class Resume extends React.PureComponent {
               <ProjectDev dev={val} key={i} />
             ))}
 
-            <h3 className="pa-subhead mt-12">UIUX Design</h3>
+            <h3 className="pa-subhead mt-12">UXUI Design</h3>
             <div className="flex flex-wrap">
               {data.project.design.map((val, i) => (
                 <ProjectDesign design={val} key={i} />
